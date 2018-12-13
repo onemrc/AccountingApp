dbad7aec1cc6da4b3f2ac111f493fec7921dc886
onrmrc <1774122996@qq.com>
2018-12-13 18:27:27 +0800

    封装数据库CURD

--- app/src/main/java/com/accounting/one/accountingapp/db/RecordBean.java
+++ app/src/main/java/com/accounting/one/accountingapp/db/RecordBean.java
@@ -1,5 +1,7 @@
 package com.accounting.one.accountingapp.db;
 
+import android.content.Intent;
+
 import java.util.UUID;
 
 /**
@@ -9,6 +11,12 @@ import java.util.UUID;
 
 public class RecordBean {
 
+    public enum RecordType{
+        RECORD_TYPE_EXPENSE,
+        RECORD_TYPE_INCOME,
+    }
+
+    private RecordType type;
     private double amount;
     private String category;
     private String remark;
@@ -20,5 +28,70 @@ public class RecordBean {
     public RecordBean() {
         uuid = UUID.randomUUID().toString();
         timeStamp = System.currentTimeMillis();
+    }
+
+
+    public int getType() {
+        if (this.type == RecordType.RECORD_TYPE_EXPENSE){
+            return 1;
+        }else {
+            return 2;
+        }
+    }
+
+    public void setType(int type) {
+        if (type == 1){
+            this.type = RecordType.RECORD_TYPE_EXPENSE;
+        }else {
+            this.type = RecordType.RECORD_TYPE_INCOME;
+        }
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
     }
 }
--- app/src/main/java/com/accounting/one/accountingapp/db/RecordDatabaseHelper.java
+++ app/src/main/java/com/accounting/one/accountingapp/db/RecordDatabaseHelper.java
@@ -1,8 +1,12 @@
 package com.accounting.one.accountingapp.db;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.LinkedList;
 
 
 /**
@@ -16,7 +20,15 @@ public class RecordDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "record";
 
-    private static final String create_record_db = "create";
+    private static final String CREATE_RECORD_DB = "create table record("
+            + "id integer primary key autoincrement,"
+            +"uuid text,"
+            +"amount double,"
+            +"remark text,"
+            +"type integer,"
+            +"category text,"
+            +"time integer,"
+            +"date date)";
 
     public RecordDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -24,11 +36,86 @@ public class RecordDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_RECORD_DB);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public void addRecord(RecordBean recordBean){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("uuid",recordBean.getUuid());
+        values.put("type",recordBean.getType());
+        values.put("category",recordBean.getCategory());
+        values.put("date",recordBean.getDate());
+        values.put("amount",recordBean.getAmount());
+        values.put("remark",recordBean.getRemark());
+        values.put("time",recordBean.getTimeStamp());
+        db.insert(DB_NAME,null,values);
+    }
+
+    public void removeRecord(String uuid){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(DB_NAME,"uuid=?",new String[]{uuid});
+    }
+
+    public void editRecord(String uuid,RecordBean record){
+        removeRecord(uuid);
+        record.setUuid(uuid);
+        addRecord(record);
+    }
+
+    public LinkedList<RecordBean> readRecords(String dateStr){
+        LinkedList<RecordBean> records = new LinkedList<>();
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select distince * from record where date = ? order by time asc",new String[]{dateStr});
+        if (cursor.moveToFirst()){
+            do {
+                String uuid = cursor.getString(cursor.getColumnIndex("uuid"));
+                int type = cursor.getInt(cursor.getColumnIndex("type"));
+                String remark = cursor.getString(cursor.getColumnIndex("remark"));
+                String category = cursor.getString(cursor.getColumnIndex("category"));
+                double amount = cursor.getDouble(cursor.getColumnIndex("amount"));
+                String date = cursor.getString(cursor.getColumnIndex("date"));
+                long timeStamp = cursor.getLong(cursor.getColumnIndex("time"));
+
+                RecordBean record = new RecordBean();
+                record.setUuid(uuid);
+                record.setAmount(amount);
+                record.setCategory(category);
+                record.setDate(date);
+                record.setRemark(remark);
+                record.setTimeStamp(timeStamp);
+                record.setType(type);
+
+                records.add(record);
+
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        return records;
+    }
+
+    public LinkedList<String> getAvaliableDate(){
+        LinkedList<String> dates = new LinkedList<>();
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select distinct * from record order by date asc",new String[]{});
+
+        if (cursor.moveToFirst()){
+            do {
+                String date = cursor.getString(cursor.getColumnIndex("date"));
+
+                if (!dates.contains(date)){
+                    dates.add(date);
+                }
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        return dates;
     }
 }
