0d43e9496d0418e53fde3fdb4eee7c8125f56cd4
onrmrc <1774122996@qq.com>
2018-12-13 19:23:31 +0800

    完成APP头部布局

--- app/src/main/java/com/accounting/one/accountingapp/MainActivity.java
+++ app/src/main/java/com/accounting/one/accountingapp/MainActivity.java
@@ -3,11 +3,21 @@ package com.accounting.one.accountingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //把ActionBar的阴影去掉
+        getSupportActionBar().setElevation(0);
+
     }
 }
